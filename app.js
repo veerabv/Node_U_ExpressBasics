1436564c2542a1cfcfe7cb807ec49680e010a6bd
veerabv <bveera0608@gmail.com>
2024-07-23 11:48:12 +0530

    use express in app.js

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const http = require("http");
 
 const express = require("express");
 
-const app = express()
+const app = express();
+app.use((req,res,next) => {
+  console.log('In the middleware');
+  next(); // this allows to the next funtion of the app if we not give this it will stop in the function only
+});
+
+app.use((req,res,next) => {
+  console.log("In another middleware");
+})
+
 const server = http.createServer(app);
+console.log("in server!!!!!!");
 
 server.listen(8080, () => {
   console.log("Server is running on port 8080");
--- readme.md
+++ readme.md
@@ -6,3 +6,21 @@ https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
 ``` 
 
 
+## Express.js ->frameWork
+
+  - handling the req and the types of files comes form the request
+
+ => aternative for Express: 
+         - vanila node.js
+         - Adonis.js
+         - Koa
+         - Sail.js
+
+
+
+## Install Express.js
+
+```sh
+npm install --save express
+```
+
